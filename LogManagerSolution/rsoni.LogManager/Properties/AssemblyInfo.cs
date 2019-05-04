72ba46a44e073260f342914d8d3b327d8f0df45d
Rajesh Soni <talk2rsoni@outlook.com>
2019-05-04 18:17:01 +0530

    .

--- LogManagerSolution/rsoni.LogManager/Properties/AssemblyInfo.cs
+++ LogManagerSolution/rsoni.LogManager/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("rsoni.LogManager")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Logging Management for .NET application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Rajesh Soni Personal")]
 [assembly: AssemblyProduct("rsoni.LogManager")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
