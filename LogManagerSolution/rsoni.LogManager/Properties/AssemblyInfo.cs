d2f9103502ced685deb561463a11e7b56761a22b
Rajesh Soni <talk2rsoni@outlook.com>
2019-05-05 17:11:43 +0530

    .

--- LogManagerSolution/rsoni.LogManager/Properties/AssemblyInfo.cs
+++ LogManagerSolution/rsoni.LogManager/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("rsoni.LogManager")]
 [assembly: AssemblyDescription("Logging Management for .NET application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Rajesh Soni Personal")]
+[assembly: AssemblyCompany("Rajesh's Personal")]
 [assembly: AssemblyProduct("rsoni.LogManager")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
