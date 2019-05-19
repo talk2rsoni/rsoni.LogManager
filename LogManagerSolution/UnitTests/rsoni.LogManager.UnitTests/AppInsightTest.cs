c005ea71e7453f66a37395741f8629fb8cafe411
Rajesh Soni <talk2rsoni@outlook.com>
2019-05-20 00:42:18 +0530

    Added AppInsightTest file

---
+++ LogManagerSolution/UnitTests/rsoni.LogManager.UnitTests/AppInsightTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using rsoni.LogManager.Common;
+
+namespace rsoni.LogManager.UnitTests
+{
+    [TestClass]
+    public class AppInsightTest
+    {
+        ILogger _logger;
+        [TestInitialize]
+        public void Initialization()
+        {
+            _logger = new AppInsightLogger("LogTest");
+        }
+
+
+        [TestMethod]
+        public void LogEntryTest()
+        {
+            _logger.LogEntry(Enums.LogType.Information, "Test Message");
+        }
+    }
+}
