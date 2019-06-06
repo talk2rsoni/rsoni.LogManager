4a4e8e577c7ba84e975e863c4c7d28795c708ceb
Rajesh Soni <talk2rsoni@outlook.com>
2019-06-06 19:51:07 +0530

    updated unit test

---
+++ LogManagerSolution/UnitTests/rsoni.LogManager.UnitTests/AppinsightLoggerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using rsoni.LogManager.Common;
+
+namespace rsoni.LogManager.UnitTests
+{
+    [TestClass]
+    public class AppinsightLoggerTest
+    {
+        ILogger _logger;
+        [TestInitialize]
+        public void Initialization()
+        {
+            _logger = new AppInsightLogger("LogTest");
+        }
+
+
+        [TestMethod]
+        public void LogEntryTest()
+        {
+            _logger.LogEntry(Enums.LogType.Information, "Test Message");
+        }
+
+        [TestMethod]
+        public void LogErrorTest()
+        {
+            _logger.LogError("Test Error");
+        }
+
+        [TestMethod]
+        public void LogExceptionTest()
+        {
+            _logger.LogError(new Exception("Test Log Exception"));
+        }
+    }
+}
